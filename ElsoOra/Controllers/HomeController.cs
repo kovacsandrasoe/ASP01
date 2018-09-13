ad29398b5302ba70d6bbaca7cf517955cb6335f7
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2018-09-13 18:51:26 +0200

    #2: Fullos program

--- ElsoOra/Controllers/HomeController.cs
+++ ElsoOra/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ElsoOra.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,36 @@ namespace ElsoOra.Controllers
         public IActionResult Hitel()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult HitelSzamolo
+            (string nev, double osszeg, double kamat, int futamido)
+        {
+            double[,] matrix = new double[futamido, 5];
+            double torleszto = osszeg / futamido;
+            double sum = 0;
+            for (int i = 0; i < futamido; i++)
+            {
+                matrix[i, 0] = i + 1;
+                matrix[i, 1] = osszeg;
+                matrix[i, 2] = osszeg * (kamat / 100);
+                matrix[i, 3] = torleszto;
+                matrix[i, 4] = torleszto + (osszeg * (kamat / 100));
+
+                sum += torleszto + (osszeg * (kamat / 100));
+
+                osszeg -= torleszto;
+            }
+
+            HitelViewModel hvm = new HitelViewModel()
+            {
+                Nev = nev,
+                Matrix = matrix,
+                Osszesen = sum
+            };
+
+            return View(hvm);
         }
 
         public IActionResult Index()
---
+++ ElsoOra/ViewModels/HitelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElsoOra.ViewModels
+{
+    public class HitelViewModel
+    {
+        public double[,] Matrix { get; set; }
+        public string Nev { get; set; }
+        public double Osszesen { get; set; }
+    }
+}
